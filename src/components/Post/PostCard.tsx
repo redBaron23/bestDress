328a77902668bc4772301dc8b04a5b6563fd43c6
Patricio Toledo <pato>
2021-11-05 01:37:05 -0300

    text

--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -1,73 +1,84 @@
-import React, { useState } from 'react';
-import { TouchableOpacity, View } from 'react-native';
-import { Avatar, Button, Card, Paragraph } from 'react-native-paper';
-import PostModel from './PostModel';
-import { CommonActions, useNavigation } from '@react-navigation/native';
-import { Dictionary } from '../../utils/dictionaries';
+import React, { useState } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { Avatar, Button, Card, Paragraph } from "react-native-paper";
+import PostModel from "./PostModel";
+import { CommonActions, useNavigation } from "@react-navigation/native";
+import { Dictionary } from "../../utils/dictionaries";
 
-const LeftContent = props => <Avatar.Icon {...props} icon="account" />
+const LeftContent = (props) => <Avatar.Icon {...props} icon="account" />;
 
 interface Props {
-    post: PostModel
+  post: PostModel;
 }
 
-function onRatedButtonPressed(setItemPressed, setOppositeItemPressed,updatePost?: () => {}) {
-    setItemPressed(prevState => !prevState);
-    setOppositeItemPressed(false);
-    //updatePost();
+function onRatedButtonPressed(
+  setItemPressed,
+  setOppositeItemPressed,
+  updatePost?: () => {}
+) {
+  setItemPressed((prevState) => !prevState);
+  setOppositeItemPressed(false);
+  //updatePost();
 }
 
+const handleNavigateToProfile = (navigation: any, searchQuery: string) => {
+  if (!searchQuery) return;
 
-
-const handleNavigateToProfile = (navigation:any,searchQuery:string) => {
-    if (!searchQuery)
-      return;
-
-    
+  //Search username
+  navigation.dispatch(
+    CommonActions.navigate({
+      name: Dictionary.SEARCH,
+      params: {
+        username: searchQuery,
+      },
+    })
+  );
+  setTimeout(() => {
     //Search username
     navigation.dispatch(
       CommonActions.navigate({
-        name: Dictionary.SEARCH,
+        name: Dictionary.OTHER_PROFILE,
         params: {
           username: searchQuery,
         },
       })
-      
-    )
-    setTimeout(() => {
-    //Search username
-    navigation.dispatch(
-        CommonActions.navigate({
-          name: Dictionary.OTHER_PROFILE,
-          params: {
-            username: searchQuery,
-          },
-        })
-      )}
-        , 200);
-  }
-
+    );
+  }, 200);
+};
 
 export default function PostCard(props: Props) {
-    const { post } = props;
+  const { post } = props;
 
-    const [isLiked, setIsLiked] = useState(false);
-    const [isDisliked, setIsDisliked] = useState(false);    
-    const navigation = useNavigation();
-    return (
-        
-        <Card style={{ width: "95%"}}>
-          <TouchableOpacity onPress={()=>{handleNavigateToProfile(navigation,post.username)}}>  <Card.Title  title={post.username} left={LeftContent} /></TouchableOpacity > 
-            <Card.Cover source={{ uri: post.picture }} />
-            <Card.Actions>
-                <Button icon={(isLiked) ? "thumb-up" : "thumb-up-outline"} onPress={() => onRatedButtonPressed(setIsLiked,setIsDisliked)}>{post.likes + ((isLiked) ? 1 : 0)}</Button>
-                <Button icon={(isDisliked) ? "thumb-down" : "thumb-down-outline"} onPress={() => onRatedButtonPressed(setIsDisliked,setIsLiked)} >{post.dislikes + ((isDisliked) ? 1 : 0)}</Button>
-            </Card.Actions>
-            <Card.Content>
-                <Paragraph>{post.description}</Paragraph>
-            </Card.Content>
-            
-        </Card>
-        
-    );
+  const [isLiked, setIsLiked] = useState(false);
+  const [isDisliked, setIsDisliked] = useState(false);
+  const navigation = useNavigation();
+  return (
+    <Card style={{ width: "95%" }}>
+      <TouchableOpacity
+        onPress={() => {
+          handleNavigateToProfile(navigation, post.username);
+        }}
+      >
+        <Card.Title title={post.username} left={LeftContent} />
+      </TouchableOpacity>
+      <Card.Cover source={{ uri: post.picture }} />
+      <Card.Actions>
+        <Button
+          icon={isLiked ? "thumb-up" : "thumb-up-outline"}
+          onPress={() => onRatedButtonPressed(setIsLiked, setIsDisliked)}
+        >
+          {post.likes + (isLiked ? 1 : 0)}
+        </Button>
+        <Button
+          icon={isDisliked ? "thumb-down" : "thumb-down-outline"}
+          onPress={() => onRatedButtonPressed(setIsDisliked, setIsLiked)}
+        >
+          {post.dislikes + (isDisliked ? 1 : 0)}
+        </Button>
+      </Card.Actions>
+      <Card.Content>
+        <Paragraph>{post.description}</Paragraph>
+      </Card.Content>
+    </Card>
+  );
 }
